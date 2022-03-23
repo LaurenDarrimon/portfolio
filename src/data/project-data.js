82ab225aefb42dfb8b12848d27693120e10f37f2
LaurenDarrimon <laurendarrimon@gmail.com>
2022-03-23 14:24:24 -0700

    update portfolio project content and media

--- src/data/project-data.js
+++ src/data/project-data.js
@@ -1,70 +1,93 @@
-
-
 export const projectData = [
-    {
-      id: 1,
-      title: "The Rubber Duck Tech Blog",
-      description: "Full-stack CMS-style blog site where developers can publish and comment, using MVC architectural structure, Handlebars.js, Sequelize, and express-session.",
-      details:"Tech blog where developers come to share, learn, and debug with each other and/or anthropomorphic ducks we find on our desks.",
-      repo_url: "https://github.com/LaurenDarrimon/tech-blog",
-      deployed_url: "https://vast-chamber-35721.herokuapp.com/",
-      image_path: "https://laurendarrimon.github.io/portfolio/assets/images/rubber-duck.png",
-      backend:false
-    },
-    {
-      id: 2,
-      title: "Map-A-Test - Find COVID Testing",
-      description: "Find nearby COVID tests sites geographically using Google Maps API and Here Developer API ",
-      details:"Once the users location is determined through geolocation or direct input, they will be able to view test site information either on the map directly or on the side panel that will appear. Both options will have the name of the test site, as well as an approximate location and phone number.",
-      repo_url: "https://github.com/LaurenDarrimon/find-covid-testing",
-      deployed_url: "https://laurendarrimon.github.io/find-covid-testing/",
-      image_path: "https://laurendarrimon.github.io/portfolio/assets/images/map-test.png",
-      backend:false
-    },
-    {
-      id: 3,
-      title: "Neighborgood! A Community Sharing App",
-      description: "Full-stack application where users can post and claim gently used items offered by the community. Uses MVC architectural structure, Bootstrap, Handlebars.js, Sequelize, and express-session.",
-      details:"We used Cloudinary for cloud storage and retrieval of user posted item photos. Cloudinary Upload widget for logged in users to post photos when they post an item.",
-      repo_url: "https://github.com/LaurenDarrimon/community-share",
-      deployed_url: "https://hidden-forest-09542.herokuapp.com/",
-      image_path: "https://laurendarrimon.github.io/portfolio/assets/images/neighborgood.png",
-      backend:false
+  {
+    id: 0,
+    title: "GIT MATCH",
+    description:
+      "Full Stack MERN social network application for developers to find collaboration and community, using React.js, Apollo GraphQL, MongoDB, and GitHub API.",
+    details:
+      "Users can match with other developers that they would like to connect and collaborate with.",
+    repo_url: "https://github.com/LaurenDarrimon/git-match",
+    deployed_url: "http://morning-sea-85016.herokuapp.com/",
+    image_path:
+      "https://laurendarrimon.github.io/portfolio/assets/images/git-match.png",
+    backend: false,
+  },
 
-    },
-    {
-      id: 4,
-      title: "Social Network API",
-      description: "API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. Uses Express.js for routing, a MongoDB database, and Mongoose ODM.",
-      details:"",
-      repo_url: "https://github.com/LaurenDarrimon/social-network-api",
-      deployed_url: "https://drive.google.com/file/d/1xjWUwbRy7Ns8pSK8XNd67bny_AQxaITt/view",
-      image_path: "https://laurendarrimon.github.io/portfolio/assets/images/social-api.png",
-      backend:true
-  
-    },
-    {
-      id: 5,
-      title: "Text Editor",
-      description: "Full-stack progressive web application (PWA) with data persistence techniques and offline capabilities for using an IndexedDB database with an idb wrapper.",
-      details:"",
-      repo_url: "https://github.com/LaurenDarrimon/text-editor",
-      deployed_url: "https://polar-temple-63530.herokuapp.com/",
-      image_path: "https://laurendarrimon.github.io/portfolio/assets/images/text-editor.png",
-      backend:false
-      
-    },
-    {
-      id: 6,
-      title: "E-Commerce Back End",
-      description: "Back end for an e-commerce site using Express.js API and Sequelize to interact with a MySQL product database.",
-      details:"",
-      repo_url: "https://github.com/LaurenDarrimon/e-commerce-back-end",
-      deployed_url: "https://drive.google.com/file/d/1O0RjBP43Ao_EWZZaeH5GSV_6-PX0tpwn/view",
-      image_path: "https://laurendarrimon.github.io/portfolio/assets/images/e-commerce.png",
-      backend:true
-    
-    },
-   
-   
-  ];
+  {
+    id: 1,
+    title: "The Rubber Duck Tech Blog",
+    description:
+      "Full-stack CMS-style blog site where developers can publish and comment, using MVC architectural structure, Handlebars.js, Sequelize, and express-session.",
+    details:
+      "Tech blog where developers come to share, learn, and debug with each other and/or anthropomorphic ducks we find on our desks.",
+    repo_url: "https://github.com/LaurenDarrimon/tech-blog",
+    deployed_url: "https://vast-chamber-35721.herokuapp.com/",
+    image_path:
+      "https://laurendarrimon.github.io/portfolio/assets/images/rubber-duck.png",
+    backend: false,
+  },
+  {
+    id: 2,
+    title: "Map-A-Test - Find COVID Testing",
+    description:
+      "Find nearby COVID tests sites geographically using Google Maps API and Here Developer API ",
+    details:
+      "Once the users location is determined through geolocation or direct input, they will be able to view test site information either on the map directly or on the side panel that will appear. Both options will have the name of the test site, as well as an approximate location and phone number.",
+    repo_url: "https://github.com/LaurenDarrimon/find-covid-testing",
+    deployed_url: "https://laurendarrimon.github.io/find-covid-testing/",
+    image_path:
+      "https://laurendarrimon.github.io/portfolio/assets/images/map-test.png",
+    backend: false,
+  },
+  {
+    id: 3,
+    title: "Neighborgood! A Community Sharing App",
+    description:
+      "Full-stack application where users can post and claim gently used items offered by the community. Uses MVC architectural structure, Bootstrap, Handlebars.js, Sequelize, and express-session.",
+    details:
+      "We used Cloudinary for cloud storage and retrieval of user posted item photos. Cloudinary Upload widget for logged in users to post photos when they post an item.",
+    repo_url: "https://github.com/LaurenDarrimon/community-share",
+    deployed_url: "https://hidden-forest-09542.herokuapp.com/",
+    image_path:
+      "https://laurendarrimon.github.io/portfolio/assets/images/neighborgood.png",
+    backend: false,
+  },
+  {
+    id: 4,
+    title: "Social Network API",
+    description:
+      "API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. Uses Express.js for routing, a MongoDB database, and Mongoose ODM.",
+    details: "",
+    repo_url: "https://github.com/LaurenDarrimon/social-network-api",
+    deployed_url:
+      "https://drive.google.com/file/d/1xjWUwbRy7Ns8pSK8XNd67bny_AQxaITt/view",
+    image_path:
+      "https://laurendarrimon.github.io/portfolio/assets/images/social-api.png",
+    backend: true,
+  },
+  {
+    id: 5,
+    title: "Text Editor",
+    description:
+      "Full-stack progressive web application (PWA) with data persistence techniques and offline capabilities for using an IndexedDB database with an idb wrapper.",
+    details: "",
+    repo_url: "https://github.com/LaurenDarrimon/text-editor",
+    deployed_url: "https://polar-temple-63530.herokuapp.com/",
+    image_path:
+      "https://laurendarrimon.github.io/portfolio/assets/images/text-editor.png",
+    backend: false,
+  },
+  {
+    id: 6,
+    title: "E-Commerce Back End",
+    description:
+      "Back end for an e-commerce site using Express.js API and Sequelize to interact with a MySQL product database.",
+    details: "",
+    repo_url: "https://github.com/LaurenDarrimon/e-commerce-back-end",
+    deployed_url:
+      "https://drive.google.com/file/d/1O0RjBP43Ao_EWZZaeH5GSV_6-PX0tpwn/view",
+    image_path:
+      "https://laurendarrimon.github.io/portfolio/assets/images/e-commerce.png",
+    backend: true,
+  },
+];
